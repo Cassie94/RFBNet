13f94e19ae81d3552ca38b555a5d0b90086c7d9b
Cassie94 <jeromechan94@gmail.com>
2018-11-25 18:29:33 -0500

    multiple_iou_for_test

--- data/voc_eval.py
+++ data/voc_eval.py
@@ -227,8 +227,8 @@ def voc_eval(detpath,
         rec_thres[thres], prec_thres[thres], ap_thres[thres] =({} for i in range(3))
         fp_size, tp_size, rec_size, prec_size, ap_size = ({} for i in range(5))
         for x,xx in zip(size_list, [1,2,3]):
-            fp_size[x] = np.cumsum(fp[size_index==xx])
-            tp_size[x] = np.cumsum(tp[size_index==xx])
+            fp_size[x] = np.cumsum(fp[thres][size_index==xx])
+            tp_size[x] = np.cumsum(tp[thres][size_index==xx])
             rec_size[x] = tp_size[x] / float(npos_size[x])
             prec_size[x] = tp_size[x] / np.maximum(tp_size[x] + fp_size[x], np.finfo(np.float64).eps)
             ap_size[x] = voc_ap(rec_size[x], prec_size[x], use_07_metric)
