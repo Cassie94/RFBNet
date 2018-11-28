a01e35842c8d982dd1a40e190b0e34584a9d6ce5
HAO CHEN <jeromechan94@gmail.com>
2018-11-27 23:16:03 -0500

    ch

--- data/voc_eval.py
+++ data/voc_eval.py
@@ -239,12 +239,16 @@ def voc_eval(detpath,
             else:
                 fp[thres][d] = 1.
 
-    size_list, score_list, iou_list, nms_list = ([] for i in range(4))
+    size_list, score_list, iou_list = ([] for i in range(3))
+    nms_list = {}
+    for thres in ovthresh:
+        nms_list[thres] = []
     for k,v in class_recs.items():
         if len(v['max_score']) > 0:
-            nms_list += v['nms_list'][0.5]
             for x,xx in zip([score_list, iou_list, size_list], ['max_score', 'max_overlap', 'size']):
                 x += list(v[xx])
+            for kk,vv in v['nms_count']:
+                nms_list[kk] += vv
 
     size_index = np.piecewise(obj_size, [obj_size<=size_range[0], \
         (obj_size>size_range[0])*(obj_size<=size_range[1]), obj_size>size_range[1]], [1,2,3])
